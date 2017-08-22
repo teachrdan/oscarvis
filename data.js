a13410ec16caa899205963e9be2d17b0950a233c
Dan Tennery-Spalding <teachrdan@gmail.com>
2017-08-21 20:26:00 -0700

    created data for 2017 Oscar winners

---
+++ data.js
@@ -0,0 +1,109 @@
+const data = {
+	bestActress: {
+		id: 001,
+		name: "Emma Stone",
+		year: 2017,
+		dob: "1988-11-06T00:00:01.000Z",
+		award: "Best Actress",
+		netWorth: 28000000,
+		portrait: "https://media.self.com/photos/585c0ea98a2a9595367fa2df/4:3/w_752,c_limit/emma-stone-anxiety.jpg",
+		winner: true
+	},
+	bestActor: {
+		id: 002,
+		name: "Casey Affleck",
+		year: 2017,
+		dob: "1975-08-12T00:00:01.000Z",
+		award: "Best Actor",
+		netWorth: 10000000,
+		portrait: "http://data.whicdn.com/images/183914301/large.jpg",
+		winner: true
+	},
+	bestSupportingActress: {
+		id: 003,
+		name: "Viola Davis",
+		year: 2017,
+		dob: "1965-08-11T00:00:01.000Z",
+		award: "Best Supporting Actress",
+		netWorth: 3000000,
+		portrait: "https://cdn.aarp.net/content/dam/aarp/entertainment/movies/2015-07/1140-viola-davis-cover.imgcache.rev0b875e6f2da0a1f93d70e4a94f23204a.jpg",
+		winner: true
+	},
+	bestSupportingActor: {
+		id: 004,
+		name: "Mahershala Ali",
+		year: 2017,
+		dob: "1974-02-16T00:00:01.000Z",
+		award: "Best Supporting Actor",
+		netWorth: 8000000,
+		portrait: "http://media2.popsugar-assets.com/files/2014/11/20/204/n/1922398/3efd61359478a4a5_thumb_temp_image10906271416541867/i/Mahershala-Ali-Mockingjay-Interview.jpg",
+		winner: true
+	},
+	bestDirector: {
+		id: 005,
+		name: "Damien Chazelle",
+		year: 2017,
+		dob: "1985-01-19T00:00:01.000Z",
+		award: "Best Director",
+		netWorth: 10000000,
+		portrait: "https://images-na.ssl-images-amazon.com/images/M/MV5BMjE2MzcyODA3MV5BMl5BanBnXkFtZTgwNTA3NTU2MjE@._V1_UY317_CR5,0,214,317_AL_.jpg",
+		winner: true
+	}
+};
+
+module.exports = data;
+//
+// const data = {
+// 	bestActress: {
+// 		id: 001,
+// 		name: "",
+// 		year: 2017,
+// 		dob: "",
+// 		award: "Best Actress",
+// 		netWorth: ,
+// 		portrait: "",
+// 		winner: true
+// 	},
+// 	bestActor: {
+// 		id: 002,
+// 		name: "",
+// 		year: 2017,
+// 		dob: "",
+// 		award: "Best Actor",
+// 		netWorth: ,
+// 		portrait: "",
+// 		winner: true
+// 	},
+// 	bestSupportingActress: {
+// 		id: 003,
+// 		name: "",
+// 		year: 2017,
+// 		dob: "",
+// 		award: "Best Supporting Actress",
+// 		netWorth: ,
+// 		portrait: "",
+// 		winner: true
+// 	},
+// 	bestSupportingActor: {
+// 		id: 004,
+// 		name: "",
+// 		year: 2017,
+// 		dob: "",
+// 		award: "Best Supporting Actor",
+// 		netWorth: ,
+// 		portrait: "",
+// 		winner: true
+// 	},
+// 	bestDirector: {
+// 		id: 005,
+// 		name: "",
+// 		year: 2017,
+// 		dob: "",
+// 		award: "Best Director",
+// 		netWorth: ,
+// 		portrait: "",
+// 		winner: true
+// 	}
+// };
+//
+// module.exports = data;
